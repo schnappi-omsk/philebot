0beaa4842ac2d128963bf5ab6912fa4f896500ba
Oleg Alekseev <schnappi.dev@gmail.com>
2024-02-03 17:10:24 +0100

    Update error message for null game title

--- src/main/java/com/gundomrays/philebot/command/PhilGameStatsCommand.java
+++ src/main/java/com/gundomrays/philebot/command/PhilGameStatsCommand.java
@@ -33,7 +33,7 @@ public class PhilGameStatsCommand implements PhilCommand {
 
         if (title == null) {
             log.warn("Null returned for titleId={}", titleId);
-            return PhilCommandUtils.textResponse(String.format("<code>Incorrect command: /game%s</code>", titleId));
+            return PhilCommandUtils.textResponse(String.format("<code>Nothing found for game %s</code>", titleId));
         }
 
         log.info("Go for leaderboard for game: {}", title.getName());
