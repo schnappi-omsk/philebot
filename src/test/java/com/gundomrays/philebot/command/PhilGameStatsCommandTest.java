61326114c52bf2820ffa55909b4d339f3b930e97
Oleg Alekseev <schnappi.dev@gmail.com>
2024-02-05 10:18:56 +0100

    Add new unit tests and remove unused code

---
+++ src/test/java/com/gundomrays/philebot/command/PhilGameStatsCommandTest.java
@@ -0,0 +1,60 @@
+package com.gundomrays.philebot.command;
+
+import com.gundomrays.philebot.data.XboxTitleHistoryDataService;
+import com.gundomrays.philebot.xbox.domain.Title;
+import com.gundomrays.philebot.xbox.xapi.XboxTitleService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+public class PhilGameStatsCommandTest {
+
+    @Mock
+    private XboxTitleHistoryDataService xboxTitleHistoryDataService;
+
+    @Mock
+    private XboxTitleService xboxTitleService;
+
+    @InjectMocks
+    private PhilGameStatsCommand philGameStatsCommand;
+
+    @Test
+    public void testExecute() {
+        // Set up test case
+        String argument = "testArgument";
+
+        CommandRequest request = new CommandRequest();
+        request.setArgument(argument);
+
+        Title title = new Title();
+        title.setName(argument);
+        title.setTitleImg("testImg");
+
+        Map<Integer, String> titleStats = new HashMap<>();
+        titleStats.put(1, "testPlayer");
+
+        // Mock behaviours
+        when(xboxTitleService.findTitle(any())).thenReturn(title);
+        when(xboxTitleHistoryDataService.getTitleStatistics(any())).thenReturn(titleStats);
+
+        // Perform test
+        CommandResponse response = philGameStatsCommand.execute(request);
+
+        // Verification
+        assertNotNull(response);
+        assertEquals("<strong>" + argument + "</strong>\n\n<code>" + "testPlayer" +
+                PhilCommandUtils.additionalSpaces("testPlayer", "testPlayer".length()) +
+                "\t:\t\t\t" + "1" + "%" + "</code>".trim(), response.getMessage().trim());
+        assertEquals("testImg", response.getMediaUrl());
+    }
+}
--- src/test/java/com/gundomrays/philebot/xbox/xapi/XApiClientTest.java
+++ src/test/java/com/gundomrays/philebot/xbox/xapi/XApiClientTest.java
@@ -3,89 +3,98 @@ package com.gundomrays.philebot.xbox.xapi;
 import com.gundomrays.philebot.xbox.domain.Activity;
 import com.gundomrays.philebot.xbox.domain.Profile;
 import com.gundomrays.philebot.xbox.domain.TitleHistory;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriBuilder;
 import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.net.URI;
+import java.util.Objects;
 import java.util.function.Function;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-@Disabled
-class XApiClientTest {
-    WebClient webClient = Mockito.mock(WebClient.class);
-    WebClient.RequestHeadersUriSpec uriSpecMock = Mockito.mock(WebClient.RequestHeadersUriSpec.class);
-    WebClient.RequestHeadersSpec headersSpecMock = Mockito.mock(WebClient.RequestHeadersSpec.class);
-    WebClient.ResponseSpec responseSpecMock = Mockito.mock(WebClient.ResponseSpec.class);
-    XApiClient client = new XApiClient(webClient);
+public class XApiClientTest {
+
+    /**
+     * This test class is written for the XApiClient class.
+     * Specifically, it tests the functionality of the method 'userByGamertag'.
+     */
 
     @Test
-    void userByGamertagExisting() {
-        String testGamertag = "testGamertag";
-        Profile expectedProfile = new Profile();
-        expectedProfile.setGamertag(testGamertag);
-        Mockito.when(webClient.get()).thenReturn(uriSpecMock);
-        Mockito.when(uriSpecMock.uri(Mockito.any(Function.class))).thenReturn(headersSpecMock);
-        Mockito.when(headersSpecMock.retrieve()).thenReturn(responseSpecMock);
-        Mockito.when(responseSpecMock.bodyToMono(Profile.class)).thenReturn(Mono.just(expectedProfile));
-        Profile profile = client.userByGamertag(testGamertag);
-        assertNotNull(profile);
-        assertEquals(testGamertag, profile.getGamertag());
+    public void testUserByGamertag() throws Exception {
+        // Create a mock WebClient
+        XApiClient xApiClient = xapiClientMock(Profile.class);
+
+        // Call the method to be tested with some input
+        Profile profile = xApiClient.userByGamertag("Gamertag");
+
+        // Using StepVerifier, verify that the profile returned is not null
+        StepVerifier.create(Mono.just(profile))
+                .expectSubscription()
+                .expectNextMatches(Objects::nonNull)
+                .verifyComplete();
     }
 
     @Test
-    void userByGamertagNonExisting() {
-        String testGamertag = "testGamertag";
-        Mockito.when(webClient.get()).thenReturn(uriSpecMock);
-        Mockito.when(uriSpecMock.uri(Mockito.any(Function.class))).thenReturn(headersSpecMock);
-        Mockito.when(headersSpecMock.retrieve()).thenReturn(responseSpecMock);
-        Mockito.when(responseSpecMock.bodyToMono(Profile.class)).thenReturn(Mono.empty());
-        assertThrows(RuntimeException.class, () -> client.userByGamertag(testGamertag));
+    void titleHistory() throws Exception {
+        // Create a mock WebClient
+        XApiClient xApiClient = xapiClientMock(TitleHistory.class);
+
+        TitleHistory titleHistory = xApiClient.titleHistory("12456");
+
+        StepVerifier.create(Mono.just(titleHistory))
+                .expectSubscription()
+                .expectNextMatches(Objects::nonNull)
+                .verifyComplete();
     }
 
     @Test
-    void titleHistoryExisting() {
-        String testXuid = "testXuid";
-        TitleHistory expectedTitleHistory = new TitleHistory();
-        Mockito.when(webClient.get()).thenReturn(uriSpecMock);
-        Mockito.when(uriSpecMock.uri(Mockito.any(Function.class))).thenReturn(headersSpecMock);
-        Mockito.when(headersSpecMock.retrieve()).thenReturn(responseSpecMock);
-        Mockito.when(responseSpecMock.bodyToMono(TitleHistory.class)).thenReturn(Mono.just(expectedTitleHistory));
-        TitleHistory titleHistory = client.titleHistory(testXuid);
-        assertNotNull(titleHistory);
+    void userActivity() throws Exception {
+        XApiClient xApiClient = xapiClientMock(Activity.class);
+
+        Activity activity = xApiClient.userActivity("123456");
+
+        StepVerifier.create(Mono.just(activity))
+                .expectSubscription()
+                .expectNextMatches(Objects::nonNull)
+                .verifyComplete();
     }
 
-    @Test
-    void titleHistoryNonExisting() {
-        String testXuid = "testXuid";
-        Mockito.when(webClient.get()).thenReturn(uriSpecMock);
-        Mockito.when(uriSpecMock.uri(Mockito.any(Function.class))).thenReturn(headersSpecMock);
-        Mockito.when(headersSpecMock.retrieve()).thenReturn(responseSpecMock);
-        Mockito.when(responseSpecMock.bodyToMono(TitleHistory.class)).thenReturn(Mono.empty());
-        assertThrows(RuntimeException.class, () -> client.titleHistory(testXuid));
-    }
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    private <T>  XApiClient xapiClientMock(Class<T> clazz) throws Exception {
+        WebClient webClientMock = mock(WebClient.class);
 
-    @Test
-    void userActivityExisting() {
-        String testXuid = "testXuid";
-        Activity expectedActivity = new Activity();
-        Mockito.when(webClient.get()).thenReturn(uriSpecMock);
-        Mockito.when(uriSpecMock.uri(Mockito.any(Function.class))).thenReturn(headersSpecMock);
-        Mockito.when(headersSpecMock.retrieve()).thenReturn(responseSpecMock);
-        Mockito.when(responseSpecMock.bodyToMono(Activity.class)).thenReturn(Mono.just(expectedActivity));
-        Activity activity = client.userActivity(testXuid);
-        assertNotNull(activity);
-    }
+        // Create a mock WebClient.RequestHeadersUriSpec
+        WebClient.RequestHeadersUriSpec requestHeadersUriSpecMock = mock(WebClient.RequestHeadersUriSpec.class);
 
-    @Test
-    void userActivityNonExisting() {
-        String testXuid = "testXuid";
-        Mockito.when(webClient.get()).thenReturn(uriSpecMock);
-        Mockito.when(uriSpecMock.uri(Mockito.any(Function.class))).thenReturn(headersSpecMock);
-        Mockito.when(headersSpecMock.retrieve()).thenReturn(responseSpecMock);
-        Mockito.when(responseSpecMock.bodyToMono(Activity.class)).thenReturn(Mono.empty());
-        assertThrows(RuntimeException.class, () -> client.userActivity(testXuid));
+        // Create a mock WebClient.ResponseSpec
+        WebClient.ResponseSpec responseSpecMock = mock(WebClient.ResponseSpec.class);
+        when(responseSpecMock.onStatus(any(), any())).thenReturn(responseSpecMock);
+
+        // When webClient.get() method is called, return our mocked RequestHeadersUriSpec
+        when(webClientMock.get()).thenReturn(requestHeadersUriSpecMock);
+
+        // When uri method is called on RequestHeadersUriSpec object, return our mocked RequestHeadersUriSpec
+        Function<UriBuilder, URI> any = any();
+        when(requestHeadersUriSpecMock.uri(any)).thenReturn(requestHeadersUriSpecMock);
+
+        // When retrieve method is called on RequestHeadersUriSpec object, return our mocked ResponseSpec
+        when(requestHeadersUriSpecMock.retrieve()).thenReturn(responseSpecMock);
+
+        // Mock the Mono<Profile> that should be returned from the API call
+        Mono<T> classMono = Mono.just(clazz.getConstructor().newInstance());
+
+        // When bodyToMono method is called on ResponseSpec object, return our mocked Mono<Profile>
+        when(responseSpecMock.bodyToMono(clazz)).thenReturn(classMono);
+
+        // Instantiate a XApiClient using the mocked WebClient
+        return new XApiClient(webClientMock);
     }
 }
